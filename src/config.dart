c01d2a1ef00e23dd70df757f7a3e44269f911646
Dylan J <dylan@tritan.gg>
2022-07-08 00:37:12 -0400

    client id accessable via client<>

--- src/config.dart
+++ src/config.dart
@@ -1,7 +1,4 @@
-import 'dart:ffi';
-
 class CONFIG {
-  static final String clientID = '771514226493489152';
   static final String token =
       "NzcxNTE0MjI2NDkzNDg5MTUy.GEr-HA.kaTe3zoFnJEGcg-mJWlx25V3N7mtgk82-ANvCM";
   static final int intents = 46823;
